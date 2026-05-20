29c20f44255d57008777a568ebfa5b7eb06722ae
Angele Vandamme <angele.vandamme31082004@gmail.com>
2026-05-20 19:48:16 +0200

    Add initial project files: .gitignore, index.html, and main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+
+const CreateWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    CreateWindow()
+
+    app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+})
